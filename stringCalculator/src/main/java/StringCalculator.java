82cad83b3430faa429412c0bfdf9c23faf9ba4d3
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-04-01 13:33:44 -0300

    Implementação de soma de 2 números

--- stringCalculator/src/main/java/StringCalculator.java
+++ stringCalculator/src/main/java/StringCalculator.java
@@ -1,9 +1,20 @@
+import org.junit.platform.commons.util.StringUtils;
+
 public class StringCalculator {
 
     public String add(String number){
         if(number.equals("")){
             return "0";
         }
+        int sum = 0;
+        if(number.contains(",")){
+            String [] numbers = number.split(",");
+            for (int i = 0; i < numbers.length; i++){
+                sum += Integer.parseInt(numbers[i]);
+            }
+            return Integer.toString(sum);
+        }
+
         return "";
     }
 }
